4053f23993058d76066a07a9f116b7476d97d40d
Steven Nunez <steven.nunez@gmail.com>
2014-04-15 11:55:39 -0400

    Finish level 6

--- lib/level_6/student.rb
+++ lib/level_6/student.rb
@@ -1,3 +1,10 @@
 module Level6
   # Create your Student class here
+  class Student
+  	attr_reader :name, :grade
+  	def initialize(name, grade)
+  		@name = name
+  		@grade = grade
+  	end
+  end
 end
