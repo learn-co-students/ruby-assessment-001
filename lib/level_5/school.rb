10b1d6fbe4dfbba67baaf68ad7081145511cb701
Steven Nunez <steven.nunez@gmail.com>
2014-04-15 11:02:04 -0400

    Finish level 5 solution

--- lib/level_5/school.rb
+++ lib/level_5/school.rb
@@ -1,3 +1,40 @@
 module Level5
-	# create your School class here
+	class School
+	@@schools = []
+	attr_accessor :name, :location, :students, :instructors
+	attr_reader :ranking
+	
+	def self.destroy_all
+		@@schools.clear
+	end
+
+	def self.all
+		@@schools
+	end
+
+	def initialize(name, location, ranking, students, instructors)
+		@name = name
+		@location = location
+		@ranking = ranking
+		@students = students
+		@instructors = instructors
+		@@schools << self
+	end
+
+	def student_enrolled?(name)
+		students.any? {|student| student[:name] == name }
+	end
+
+	def set_ranking(rank)
+		@ranking = rank
+	end
+
+	def add_student(name, grade)
+		students << { name: name, grade: grade }
+	end
+
+	def remove_student(name)
+		students.reject! { |student| student[:name] == name }
+	end
+end
 end
