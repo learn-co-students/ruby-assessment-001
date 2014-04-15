619b8ade40e4e0ff19c6fc29f07a13425b06d481
Steven Nunez <steven.nunez@gmail.com>
2014-04-15 12:23:30 -0400

    Add tests for Level 7

---
+++ spec/level_7_spec.rb
@@ -0,0 +1,38 @@
+describe "Level 7" do
+	describe "self" do
+		it "returns a value from a class method" do
+			# Define a class with a class method that returns self
+			class Student
+				def self.return_self
+					self
+				end
+			end
+
+			found_self = Student.return_self
+			# Fill in the blank
+			expect(found_self).to ___
+		end 
+
+		it "returns self from body of the class" do
+			$found_self = nil
+
+			class Student 
+				$found_self = self
+			end
+			# Fill in the blank
+			expect(found_self).to ___
+		end
+
+		it "returns self from an instance method" do
+			class Student
+				def return_self
+					self
+				end
+			end
+
+			student = Student.new
+			# Fill in the blank
+			expect(student.return_self).to ___
+		end
+	end
+end
